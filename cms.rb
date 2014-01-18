b24949d6caea19fecf69aacaebb8ec9fffa6ef4f
Michael Atlin <mike.atlin@gmail.com>
2014-01-17 23:53:46 -0500

    Find ID working from CMS

--- cms.rb
+++ cms.rb
@@ -6,43 +6,34 @@ class CMS
     @rolodex = Rolodex.new
   end
   
-  def print_main_menu
-    puts "[1] Add a new contact"
-    puts "[2] Modify an existing contact"
-    puts "[3] Delete a contact"
-    puts "[4] Display all the contacts"
-    puts "[5] Display an attribute"
-    puts "[6] Exit"
-    puts "Enter a number: "
-  end
-
   def main_menu
-    print_main_menu
+    puts "[1] Add a new contact"
+    puts "[2] Find a contact by their ID"
+    puts "[3] Delete a contact"
+    puts "[3] Filter contacts by attribute"
+    puts "[4] Exit"
+    puts "Enter a number: "
+
     selection = gets.chomp.to_i
     call_option(selection)
   end
   
   def call_option(selection)
     case selection
-    when 1 then new_contact
+    when 1 then @rolodex.new_contact
+    when 2 then find_id
+    when 4 then return
 
       
     end
-    
+    main_menu
   end
 
-  def new_contact
-    puts "What is your contact's name?"
-    name = gets.chomp
-    age = false
-    puts "What is your contact's age?"
-    age = gets.chomp.to_i
-    puts "What is your contact's e-mail"
-    email = gets.chomp
-    contact = @rolodex.new_contact(name, age, email)
-    puts "#{contact.name} has been added to your rolodex."
+  def find_id
+    contact = @rolodex.find_id
+    puts "ID: #{contact[:object].id}"
+    puts "Name: #{contact[:object].name}"
+    puts "Age: #{contact[:object].age}"
+    puts "Email: #{contact[:object].email}"
   end
 end
-
-cms =CMS.new
-cms.main_menu
--- rolodex.rb
+++ rolodex.rb
@@ -6,15 +6,30 @@ class Rolodex
     @id = 1000
   end
   
-  def new_contact(name, age, email)
-    @id += 1
+  def new_contact
+    puts "What is your contact's name?"
+    name = gets.chomp
 
+    puts "What is your contact's age?"
+    age = gets.chomp.to_i
+
+    puts "What is your contact's e-mail"
+    email = gets.chomp
+
+    @id += 1
     @contacts << Contact.new(@id, name, age, email)
-    @contacts.last
+
+    puts "#{@contacts.last.name} has been added to your rolodex."
   end
 
-  def find_id(id)
-    return nil if @contacts.empty?
+  def find_id
+    if @contacts.empty?
+      puts "You have no contacts in your Rolodex."
+      return nil
+    end
+
+    puts "Enter the ID of the entry you wish to view or modify."
+    id = gets.chomp.to_i
     
     i=0
     @contacts.each do |contact|
