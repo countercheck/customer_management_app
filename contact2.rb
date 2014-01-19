0cbf9f38c1d896b4e915bf111aa68ffe7979e6d9
Michael Atlin <mike.atlin@gmail.com>
2014-01-18 22:42:05 -0500

    modify_name _age and _email methods added to contact2, rather than rolodex2.

--- contact2.rb
+++ contact2.rb
@@ -17,4 +17,22 @@ class Contact
     puts "Press any key to continue"
     gets
   end
+
+  def modify_name
+    puts "Current name is #{@name}.  Please enter new name."
+    @name = gets.chomp
+    contact
+  end
+
+  def modify_age
+    puts "Current age is #{@age}.  Please enter new age."
+    @age = gets.chomp
+    contact
+  end
+
+  def modify_email
+    puts "Current email is #{@email}.  Please enter new e-mail."
+    @email = gets.chomp
+    contact
+  end
 end
