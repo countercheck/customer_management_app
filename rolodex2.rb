9e9f0ee0f7afbb8a79de4dc8bfe9c66696c19d10
Michael Atlin <mike.atlin@gmail.com>
2014-01-18 22:55:58 -0500

    rolodex.new_contact added and working

--- rolodex2.rb
+++ rolodex2.rb
@@ -5,4 +5,11 @@ class Rolodex
     @contacts = []
     @id = 1000
   end
+
+  #accepts a hash named contact(:name :age :email)
+  def new_contact(contact) 
+    @id += 1
+    @contacts << Contact.new(@id, contact[:name], contact[:age], contact[:email])
+    @contacts.last
+  end
 end
